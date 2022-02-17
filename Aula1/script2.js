bca6eae124f15b1bdd9e582889950d6a4f36dfe3
Andr3Flor3s <andredemarquesflores@gmail.com>
2022-02-17 20:58:24 -0300

    aula 1

---
+++ Aula1/script2.js
@@ -0,0 +1,46 @@
+/*//Qual o tipo de variável abaixo? R. undefined
+let weight
+//_____________________________________________
+
+//Diferentes tipos de variáveis.
+let name = 'André' //string
+let age = 23 //number
+let star = 4.5 //float
+let inSubcribed = true //boolean*/
+//______________________________________________
+
+//Qual tipo a variável abaixo? R. object
+let student = {
+    name:'André',
+    age:23,
+    weight: 96,
+    inSubcribed: true,
+}
+//console.log(`${student.name} tem ${student.age} anos de idade e pesa ${student.weight}Kg.`)
+//_______________________________________________
+
+//Declare uma variável do tipo array, de nome student e atibua a ela nenhum valor.
+//let students = []
+//_______________________________________________
+
+//Reatribua valor para a variável acima, colocando dentro dela o obeto student da questão 3.
+//let students = [
+//    student
+//]
+//console.log(students)
+//_______________________________________________
+
+//Coloque no console o valor da posição 0 do array acima.
+//console.log(students[0])
+//_______________________________________________
+
+//Crie um novo student e coloque na posição do array students.
+let john = {
+    name: jonh,
+    age: 20,
+    weight:85,
+    inSubcribed: false
+}
+
+students[1] = john
+console.log(students)
