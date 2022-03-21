b75e6a859365d0b681bec9080e2d111fd2768adc
Andr3Flor3s <andredemarquesflores@gmail.com>
2022-03-21 20:04:56 -0300

    aula 2

---
+++ Aula2/notaescola.js
@@ -0,0 +1,16 @@
+let nota = ''
+
+if (nota >= 90) {
+    console.log('Nota A')
+}else if (nota >= 80) {
+    console.log('Nota B')
+}else if (nota >= 70){
+    console.log('Nota C')
+}else if (nota >= 60){
+    console.log('Nota D')
+}else if (nota){
+    console.log('Nota F')
+}
+
+console.log(nota)
+
